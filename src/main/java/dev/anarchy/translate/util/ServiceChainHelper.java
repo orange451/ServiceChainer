5fa19a2d754473169f37b2e11b5a8591e4716e41
Andrew Hamilton <ahamilton@manh.com>
2022-02-10 14:38:41 -0500

    Create way to have default metadata for service chain

--- src/main/java/dev/anarchy/translate/util/ServiceChainHelper.java
+++ src/main/java/dev/anarchy/translate/util/ServiceChainHelper.java
@@ -13,11 +13,13 @@ public class ServiceChainHelper {
 		serviceChain.setColor(getDefaultServiceChainColor());
 		serviceChain.setHandlerId(getDefaultServiceChainHandlerId());
 		serviceChain.setLastInput(getDefaultServiceChainLastInput());
+		serviceChain.setSize(getDefaultServiceChainElementWidth(), getDefaultServiceChainElementHeight());
 		
 		int element = 0;
 		for (DRouteElementI route : serviceChain.getRoutesUnmodifyable()) {
 			double spacing = getDefaultServiceDefinitionY() - getDefaultServiceChainElementY();
 			route.setPosition(getDefaultServiceDefinitionX(), getDefaultServiceDefinitionY() + (spacing*element));
+			route.setSize(getDefaultServiceDefinitionWidth(), getDefaultServiceDefinitionHeight());
 			route.setColor(getDefaultServiceDefinitionColor());
 			route.setName(route.getDestination());
 			element = element + 1;
@@ -34,6 +36,14 @@ public class ServiceChainHelper {
 	
 	public static double getDefaultServiceChainElementY() {
 		return 880.0;
+	}
+	
+	public static double getDefaultServiceChainElementWidth() {
+		return 140.0;
+	}
+	
+	public static double getDefaultServiceChainElementHeight() {
+		return 80.0;
 	}
 
 	public static String getDefaultServiceChainName() {
@@ -58,5 +68,13 @@ public class ServiceChainHelper {
 	
 	public static double getDefaultServiceDefinitionY() {
 		return 1020.0;
+	}
+	
+	public static double getDefaultServiceDefinitionWidth() {
+		return 220.0;
+	}
+	
+	public static double getDefaultServiceDefinitionHeight() {
+		return 60.0;
 	}
 }
