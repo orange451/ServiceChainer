4431a4dd042a413bfe1c35a4e67d95113b9fb40d
Andrew Hamilton <ahamilton@manh.com>
2022-02-10 15:33:27 -0500

    Additional missing JsonIgnorePrioerties

--- src/main/java/dev/anarchy/common/DDestinationParams.java
+++ src/main/java/dev/anarchy/common/DDestinationParams.java
@@ -1,7 +1,9 @@
 package dev.anarchy.common;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DDestinationParams {
 
 	@JsonProperty("ParameterName")
